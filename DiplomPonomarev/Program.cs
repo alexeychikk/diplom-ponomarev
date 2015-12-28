a6e6f900da9c927a51ebd26da977ed8e29f3207f
Зинкевич Алексей <alexzinkevych@yandex.ua>
2015-12-28 18:29:33 +0200

    Hosting wpf component init

--- DiplomPonomarev/Program.cs
+++ DiplomPonomarev/Program.cs
@@ -8,24 +8,12 @@ namespace DiplomPonomarev
 {
     static class Program
     {
+        [STAThread]
         static void Main()
         {
-            using (Form1 frm = new Form1())
-            {
-                if (!frm.InitializeGraphics())
-                {
-                    MessageBox.Show("Помилка ініціалізації Direct3D!\nНатисніть ОК для завершення програми.", "Direct3D Error",
-                        MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    return;
-                }
-                frm.Show();
-
-                while (frm.Created)
-                {
-                    frm.Render();
-                    Application.DoEvents();
-                }
-            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
     }
 }
